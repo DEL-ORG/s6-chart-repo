ecc781f5ca67919d2e59133aefc1383aeab641c2
devopseasylearning <info@devopseasylearning.com>
2024-03-14 15:49:44 -0500

    added script to create  a private repo and deploy kuztomize

---
+++ deploy-chart-for-argocd.sh
@@ -0,0 +1,3 @@
+argocd repo add git@github.com:DEL-ORG/s6-revive-chart-repo.git --ssh-private-key-path ~/.ssh/id_rsa
+
+kubectl apply -k /home/peterg/Documents/devops-learnninng/s6-classwork/eric-Revive-project-/phase12/phase-12-group/s6-revive-chart-repo/revive-project
