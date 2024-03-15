af013cc835dadc20dac86b682371e33515c8d57c
devopseasylearning <info@devopseasylearning.com>
2024-03-15 18:04:19 -0500

    reset repos

--- deploy-chart-for-argocd.sh
+++ deploy-chart-for-argocd.sh
@@ -7,4 +7,11 @@ sleep 5
 
 ##argocd proj patch revive --src git@github.com:DEL-ORG/s6-revive-chart-repo.git
 
+
+#deploy with kuztomized
+
 kubectl apply -k /home/peterg/Documents/devops-learnninng/s6-classwork/eric-Revive-project-/phase12/phase-12-group/s6-revive-chart-repo/revive-project
+
+#argocd app sync revive --prune
+
+
