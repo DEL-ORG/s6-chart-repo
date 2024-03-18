95b3f490abdc06c2560fbd27bcaa469217166a0b
Prince <opokuprince21@gmail.com>
2024-03-17 22:36:44 -0600

    Update deploy-chart-for-argocd.sh

--- deploy-chart-for-argocd.sh
+++ deploy-chart-for-argocd.sh
@@ -1,63 +1,23 @@
-
-
-#create private repo in argocd 
-argocd repo add git@github.com:DEL-ORG/s6-revive-chart-repo.git --ssh-private-key-path ~/.ssh/id_rsa
-
-
-
-
-#create argocd project and restrict to repo git@github.com:DEL-ORG/s6-revive-chart-repo.git
+#! /bin/bash
 cat <<EOF | kubectl apply -f -
-
 apiVersion: argoproj.io/v1alpha1
-kind: AppProject
+kind: Application
 metadata:
   name: revive
   namespace: argocd
-  # Finalizer that ensures that project is not deleted until it is not referenced by any application
-#   finalizers:
-#     - resources-finalizer.argocd.argoproj.io
 spec:
-  # Project description
-  description: Revive  Project
-
-  # Allow manifests to deploy from olny Git repos git@github.com:DEL-ORG/s6-revive-chart-repo.git
-  sourceRepos:
-  - git@github.com:DEL-ORG/s6-revive-chart-repo.git
-
-  # Only permit applications to deploy to the revive namespace in the same cluster
-  # Destination clusters can be identified by 'server', 'name', or both.
-  destinations:
-  - namespace: revive
+  destination:
+    namespace: revive
     server: https://kubernetes.default.svc
-    name: in-cluster
-   
-  # Sync windows restrict when Applications may be synced. https://argo-cd.readthedocs.io/en/stable/user-guide/sync_windows/
-  syncWindows:
-  - kind: allow
-    schedule: '* * * * *'
-    clusters:
-      - in-cluster
-      
-
-
+  project: revive
+  source:
+    path: revive-project/
+    repoURL: 'git@github.com:DEL-ORG/s6-revive-chart-repo.git'
+    targetRevision: develop
+  syncPolicy:
+    syncOptions:
+      - CreateNamespace=true
+    automated:
+      prune: true
+      selfHeal: true
 EOF
-
-sleep 3
-
-
-#use kustomized to deploy heml charts 
-kubectl apply -k /home/peterg/Desktop/Projects/revive/phase12/phase-12-group/s6-revive-chart-repo/revive-project
-
-
-
-
-
-
-
-
-
-
-
-
-
