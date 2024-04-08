820c65aab914187425ea8487d42f13bf9b1b1e38
opk <opk@example.com>
2024-04-07 22:23:49 -0600

    Added a new helm chart

--- image-pull-secret.sh
+++ image-pull-secret.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 
-kubectl create secret docker-registry ecr-revive \
-  --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
-  --docker-username=AWS \
-  --docker-password="$(aws ecr get-login-password --region us-east-1)"
-
-
-# NAMESPACE_NAME="revive" && \
-# kubectl create namespace $NAMESPACE_NAME || true && \
 # kubectl create secret docker-registry ecr-revive \
-#   --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
 #   --docker-username=AWS \
-#   --docker-password=$(aws ecr get-login-password) \
-#   --namespace=$NAMESPACE_NAME
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=assets
+
+# kubectl create secret docker-registry ecr-revive \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
+#   --docker-username=AWS \
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=carts
+
+# kubectl create secret docker-registry ecr-revive \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
+#   --docker-username=AWS \
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=catalog
+
+# kubectl create secret docker-registry ecr-revive \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
+#   --docker-username=AWS \
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=checkout
+
+# kubectl create secret docker-registry ecr-revive \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
+#   --docker-username=AWS \
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=orders
+
+# kubectl create secret docker-registry ecr-revive \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
+#   --docker-username=AWS \
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=rabbitmq
+
+# kubectl create secret docker-registry ecr-revive \
+#   --docker-server=637423375996.dkr.ecr.us-east-1.amazonaws.com \
+#   --docker-username=AWS \
+#   --docker-password="$(aws ecr get-login-password --region us-east-1)" \
+#   --namespace=ui
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 637423375996.dkr.ecr.us-east-1.amazonaws.com
